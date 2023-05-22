f9d85e069135566aa19119c6dc695c57c1684d85
Lukáš Huvar <lukas@huvar.cz>
2023-05-22 13:11:07 +0200

    Update deps

--- .graphqlrc.js
+++ .graphqlrc.js
@@ -1,7 +1,11 @@
 /** @type {import('@graphql-codegen/cli').CodegenConfig} */
 const config = {
   schema: 'src/lib/graphql/schema.graphql',
-  documents: ['src/pages/**.tsx', 'src/components/**.tsx', '!src/gql/**/*'],
+  documents: [
+    'src/pages/**.tsx',
+    'src/components/**.tsx',
+    '!src/lib/graphql/**/*',
+  ],
   generates: {
     './src/lib/graphql/': {
       preset: 'client',
